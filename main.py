f8fbceb6e4d46e813d16ae02472769ced3456d64
YanginFire <90453727+YanginFire@users.noreply.github.com>
2025-10-06 16:20:11 +0000

    changes in code

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from sympy import *
 
-
 # 1 способ
-k, T, C, L = symbols('k C T L') 
+k, T, C, L = symbols('k C T L')
 
 C_ost = 90000
-Am_lst = [ ] 
-C_ost_lst = [ ] 
+Am_lst = []
+C_ost_lst = []
 
 for i in range(9):
   Am = (C - L) / T
-  C_ost -= Am.subs({C: 90000, 
-                    T: 9, 
-                    L: 0
-                   })
-  Am_lst.append(round(Am.subs({C: 90000, 
-                               T: 9, 
-                               L: 0}), 
-                               2))
+  C_ost -= Am.subs({C: 90000, T: 9, L: 0})
+  Am_lst.append(round(Am.subs({C: 90000, T: 9, L: 0}), 2))
   C_ost_lst.append(round(C_ost, 2))
 
 print('Am_lst:', Am_lst)
@@ -32,7 +25,7 @@ for i in range(5):
   Am = k * 1 / T * (C - Aj)
   C_ost -= Am.subs({C: 90000, k: 2, T: 9})
   Am_lst_2.append(round(Am.subs({C: 90000, k: 2, T: 9}), 2))
-  Aj += Am 
+  Aj += Am
   C_ost_lst_2.append(round(C_ost, 2))
 print('Am_lst_2:', Am_lst_2)
 print('C_ost_lst_2:', C_ost_lst_2)
@@ -40,13 +33,13 @@ print('C_ost_lst_2:', C_ost_lst_2)
 # Контейнер табличного вывода
 import pandas as pd
 
-Y = range(1,9)
+Y = range(1, 9)
 table1 = list(zip(Y, C_ost_lst, Am_lst))
 table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
 tfame = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
 tfame2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
 
-print(tfame) 
+print(tfame)
 print(tfame2)
 
 # Контейнер визуализации
@@ -55,15 +48,25 @@ from matplotlib import pyplot as plt
 
 # Line chart
 plt.plot(tfame['Y'], tfame['C_ost_lst'], label='Am')
-plt .plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am_2')
+plt.plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am_2')
 plt.show()
 
-# Pie chart 
+# Pie chart
 vals = Am_lst_2
-labels = list(range(1,9))
-explode = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,  0.1]
+labels = list(range(1, 9))
+explode = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 fig, ax = plt.subplots()
-ax.pie(vals, labels=labels, explode=explode, autopct='%1.1f%%', shadow=True, rotatelabels=True, wedgeprops={'edgecolor': 'k', 'lw':1, 'ls':'--'})
+ax.pie(vals,
+       labels=labels,
+       explode=explode,
+       autopct='%1.1f%%',
+       shadow=True,
+       rotatelabels=True,
+       wedgeprops={
+           'edgecolor': 'k',
+           'lw': 1,
+           'ls': '--'
+       })
 ax.axis('equal')
 plt.show()
 
@@ -78,8 +81,6 @@ plt.show()
 
 plt.bar(tfame['Y'], tfame2['Am_lst_2'])
 plt.show()
-
-
 
 # Вариант 9
 
@@ -117,7 +118,7 @@ print('C_ost_lst_2:', C_ost_lst_2)
 # Контейнер табличного вывода
 import pandas as pd
 
-Y = range(1,9)
+Y = range(1, 9)
 table1 = list(zip(Y, C_ost_lst, Am_lst))
 table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
 tfame = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
@@ -132,15 +133,25 @@ from matplotlib import pyplot as plt
 
 # Line chart
 plt.plot(tfame['Y'], tfame['C_ost_lst'], label='Am')
-plt .plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am_2')
+plt.plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am_2')
 plt.show()
 
 # Pie chart
 vals = Am_lst_2
-labels = list(range(1,10))
-explode = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+labels = list(range(1, 10))
+explode = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 fig, ax = plt.subplots()
-ax.pie(vals, labels=labels, explode=explode, autopct='%1.1f%%', shadow=True, rotatelabels=True, wedgeprops={'edgecolor': 'k', 'lw':1, 'ls':'--'})
+ax.pie(vals,
+       labels=labels,
+       explode=explode,
+       autopct='%1.1f%%',
+       shadow=True,
+       rotatelabels=True,
+       wedgeprops={
+           'edgecolor': 'k',
+           'lw': 1,
+           'ls': '--'
+       })
 ax.axis('equal')
 plt.show()
 
